1e3eb6997959a294fe37c0583e61e670d85ddccf
Asad122807 <2230845@iub.edu.bd>
2026-08-06 00:22:31 +0600

    Initial commit

---
+++ src/main/java/com/summer26/section3/group29/simulatingoperationsofbangladeshbank/Launcher.java
@@ -0,0 +1,9 @@
+package com.summer26.section3.group29.simulatingoperationsofbangladeshbank;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.summer26.section3.group29.simulatingoperationsofbangladeshbank {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.summer26.section3.group29.simulatingoperationsofbangladeshbank to javafx.fxml;
+    exports com.summer26.section3.group29.simulatingoperationsofbangladeshbank;
+}
