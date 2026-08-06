33eb9f772eb90ceba8d31a91d9d080380d840727
Asad122807 <2230845@iub.edu.bd>
2026-08-07 04:06:45 +0600

    commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,8 @@ module com.summer26.section3.group29.simulatingoperationsofbangladeshbank {
     requires javafx.controls;
     requires javafx.fxml;
 
-    // Open main package to JavaFX
-    opens com.summer26.section3.group29.simulatingoperationsofbangladeshbank to javafx.fxml;
 
-    // OPEN THE SUB-PACKAGE 'Asad' TO JAVAFX
+    opens com.summer26.section3.group29.simulatingoperationsofbangladeshbank to javafx.fxml;
     opens com.summer26.section3.group29.simulatingoperationsofbangladeshbank.Asad to javafx.fxml;
 
     exports com.summer26.section3.group29.simulatingoperationsofbangladeshbank;
